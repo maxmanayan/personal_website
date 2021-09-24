206a07728fdcd1ba0d92d9005274cc8f3e5ac394
Max Manayan <maximillian521@gmail.com>
2021-09-24 11:41:02 -0600

    basic no match page setup with link to home

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NoMatch = () => {
   return (
     <div id="no-match">
-      <h1>No Match</h1>
+      <h2>The page you are looking for does not exist!</h2>
+      <div className="return-home">
+        <h2>Return</h2>
+        <Link to="/" className="link">
+          <h2>Home</h2>
+        </Link>
+      </div>
     </div>
   );
 };
